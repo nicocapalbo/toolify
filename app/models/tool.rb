d054248d1461422c717f3314e64b93bee08173c3
Alejandro Bringas <bringasalejandro@gmail.com>
2020-06-16 14:09:02 +0200

    Tools controller and views

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,6 +1,5 @@
 class Tool < ApplicationRecord
   belongs_to :user
-  belongs_to :booking
   validates :name, :description, :price, presence: true
-  validates :price, numericality: { only_integer: true } 
+  validates :price, numericality: { only_integer: true }
 end
