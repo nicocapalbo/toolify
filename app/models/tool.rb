b7a2d9c8d786b511dbd42e71967acd23b78a6e26
Alejandro Bringas <bringasalejandro@gmail.com>
2020-06-16 15:31:44 +0200

    Active storage and cloudinary setup

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,5 +1,6 @@
 class Tool < ApplicationRecord
   belongs_to :user
+  has_many_attached :photos
   validates :name, :description, :price, presence: true
   validates :price, numericality: { only_integer: true }
 end
