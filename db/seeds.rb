9f720bb35883179cb547d2f7d7332900bb81db42
Alejandro Bringas <bringasalejandro@gmail.com>
2020-06-16 17:05:30 +0200

    Added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "1. Creating users..."
+
+User.create(first_name: "Alejandro", last_name: "Bringas", email: "alejandro@mail.com", password: "123456")
+User.create(first_name: "Nicolas", last_name: "Capalbo", email: "nicolas@mail.com", password: "123456")
+User.create(first_name: "David", last_name: "Ibañez", email: "david@mail.com", password: "123456")
+User.create(first_name: "Francesco", last_name: "Biedermann", email: "francesco@mail.com", password: "123456")
+
+puts "2. Creating tools..."
+
+20.times { Tool.create( name: Faker::Construction.heavy_equipment, description: "Some details", location: "Barcelona", price: rand(1..20), user_id: rand(1..4))}
+
+puts "3. Process completed"
