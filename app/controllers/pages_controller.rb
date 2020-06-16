aaf09d0b27401dc73af4046d96a7c6050c5d20d1
Francesco Biedermann <francesco.biedermann@outlook.de>
2020-06-16 17:56:27 +0200

    made homepage dynamic & deleted home link

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @tools = Tool.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,23 @@
 
 puts "1.Cleaning database..."
 Tool.destroy_all
+User.destroy_all
 
-puts "2.Creating tools..."
-screwdriver = { name: "Screwdriver", description: "best screwdriver in the world", price: "5" }
-hammer = { name: "Hammer", description: "best hammer in the world", price: "3" }
+puts "2.Creating users..."
+david = User.create(first_name: "David", last_name: "Ibañez", email: "david@gmail.com", password: "123456")
+ale = User.create(first_name: "Ale", last_name: "Bringas", email: "ale@gmail.com", password: "123456")
+
+puts "3.Creating tools..."
+screwdriver = { name: "Screwdriver", description: "best screwdriver in the world", price: 5}
+hammer = { name: "Hammer", description: "best hammer in the world", price: 3 }
+saw = { name: "Saw", description: "best saw in the world", price: 4 }
+key = { name: "Key", description: "best key in the world", price: 2 }
 
 counter = 3
-[ screwdriver, hammer].each do |attributes|
-  tool = Tool.create!(attributes)
+[ screwdriver, hammer, saw, key].each do |attributes|
+  tool = Tool.new(attributes)
+  tool.user = User.all.sample
+  tool.save
   puts "#{counter += 1} - Created #{tool.name}"
 end
 puts "#{counter + 1}- Finished!"
