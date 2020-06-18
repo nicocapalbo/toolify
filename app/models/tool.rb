23c5469c8cbc18a901bce45594337ee1b9524842
Francesco Biedermann <francesco.biedermann@outlook.de>
2020-06-18 12:04:35 +0200

    geocoded address for tools

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -3,4 +3,6 @@ class Tool < ApplicationRecord
   has_one_attached :photo
   validates :name, :description, :price, presence: true
   validates :price, numericality: { only_integer: true }
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
